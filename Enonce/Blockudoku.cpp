815db54e5c14ffdea2b0e35d7908bfdb24d83a4f
Blackh0le <r43206616@gmail.com>
2021-03-24 00:21:02 +0100

    :fire: :smile: Update threadCases

--- Enonce/Blockudoku.cpp
+++ Enonce/Blockudoku.cpp
@@ -77,14 +77,28 @@ pthread_t thHandler;
 struct timespec monTimer;
           
 // === Déclaration des mutex ===
-pthread_mutex_t mutexMessage        = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutexScore          = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutexCaseInserees   = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexMessage         = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexScore           = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexCasesInserees   = PTHREAD_MUTEX_INITIALIZER;
 
 // === Déclaration des variables condition ===
 pthread_cond_t	condScore;
 pthread_cond_t  condCasesInserees;
 
+
+
+int lignesCompletes[NB_CASES];
+int nbLignesCompletes;
+int colonnesCompletes[NB_CASES];
+int nbColonnesCompletes;
+int carresComplets[NB_CASES];
+int nbCarresComplets;
+int nbAnalyses;
+
+pthread_key_t cle;
+
+pthread_t threadHandle;
+pthread_t tabThreadCase[9][9];
 
 // === Les variables de travail ===
 char* message;		// Pointeur vers le message à faire défiler
@@ -92,7 +106,7 @@ int   tailleMessage;	// Longueur du message
 int   indiceCourant;	// Indice du premier caractère à afficher dans la zone graphique
 
 int  score		  = 0;
-bool MAJScore	  = false;
+bool MAJScore	  = true;
 
 
 PIECE pieceEnCours; 
@@ -107,10 +121,11 @@ void  *threadEvent(void);
 void  *threadScore(void);
 void  *threadEvent(void);
 void  *threadPiece(void);
+void  *threadCases(CASE *);
 
 // === Les prototypes des fonctions ===
 void  HandlerSIGALARM(int sig);
-void  HandlerClickGauche(int sig);
+void  HandlerSIGUSR1(int sig);
 void  HandlerClickDroit(int sig);
 
 
@@ -123,6 +138,7 @@ void  CreerPiece();
 int   RandomCouleurPiece(void);
 void  RotationPiece(PIECE *pPiece);
 
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc,char* argv[])
 {
@@ -130,7 +146,9 @@ int main(int argc,char* argv[])
   /* ------------------------------------------------------------
    DECLARATION POUR L'UTILISATION DES SIGNAUX
    ------------------------------------------------------------ */
-  
+  unsigned short index = 0;
+  int l = 0, c = 0;
+  CASE *pCase = NULL;
   struct sigaction sigact;
   
   
@@ -154,7 +172,7 @@ int main(int argc,char* argv[])
   
   // === Initialise le mutex avec les parametres par defaut ===
   pthread_mutex_init(&mutexMessage, NULL);
-  pthread_mutex_init(&mutexCaseInserees, NULL);
+  pthread_mutex_init(&mutexCasesInserees, NULL);
   pthread_mutex_init(&mutexScore, NULL);
   
   
@@ -184,8 +202,24 @@ int main(int argc,char* argv[])
   setMessage("Bienvenue sur Blockudoku", true);
 
 
-  pause();
-  pause();
+  for(l = 0; l < 9; l++)
+  {
+    for(c = 0; c < 9; c++)
+    {
+      pCase = (CASE *) malloc(sizeof(CASE));
+      pCase->ligne = l;
+      pCase->colonne = c;
+      phtrad_create(&tabThreadCase[l][c], NULL, (void *(*)(void *))threadCases, CaseThread);
+    }
+  }
+
+  for(index = 0; index < NB_CASES; ++index)
+  {
+    lignesC
+  }
+
+
+  while(1);
 
   // Fermeture de la fenetre
   printf("(MAIN %d) Fermeture de la fenetre graphique...",pthread_self()); fflush(stdout);
@@ -287,65 +321,142 @@ void RotationPiece(PIECE *pPiece)
 
 
 
-void *threadPiece(void)
+void * threadPiece(void)
 {
-  // Déclaration des variables de travail
+
   srand(time(NULL));
 
-  unsigned short indiceRotation = 0;
-  unsigned short index          = 0;
-  unsigned short nbRotations    = rand() % 4; // 0 -> 3 
-  bool running                  = true;
-  int LMin = 0, CMin = 0;
+  // Déclaration des variables de travail
+  int i, LMin, CMin;
+  
+  bool estSimilairePiece; // Piece en cours = Piece insérée
+  unsigned short nbRotations = 0, indexRotation = 0;
+  unsigned short MainRunning = 1, SecondRunning;
 
-  while(running)
+
+  sigset_t mask;
+  sigfillset(&mask); // On masque l'ensemble des signaux
+  sigprocmask(SIG_SETMASK, &mask, NULL);
+
+  
+
+  while(MainRunning)
   {
-    
     CreerPiece();
-    while(indiceRotation < nbRotations)
+
+    nbRotations = rand() % 4; // 0 -> 3
+    while(indexRotation < nbRotations)
     {
       RotationPiece(&pieceEnCours);
-      indiceRotation++;
-    }
-    TriCases(pieceEnCours.cases, 0, pieceEnCours.nbCases); // vecteur, indiceDebut, indiceFin
-
-    for(indiceRotation = 0; indiceRotation < pieceEnCours.nbCases; ++indiceRotation)
-    {
-        pieceEnCours.cases[indiceRotation].ligne   = pieceEnCours.cases[indiceRotation].ligne + 3;
-        pieceEnCours.cases[indiceRotation].colonne = pieceEnCours.cases[indiceRotation].colonne + 14;
-        //fprintf(stderr, "\x1b[32m(%d, %d)\x1b[0m\n", pieceEnCours.cases[indiceRotation].ligne, pieceEnCours.cases[indiceRotation].colonne);
-        DessineDiamant(pieceEnCours.cases[indiceRotation].ligne, pieceEnCours.cases[indiceRotation].colonne, pieceEnCours.couleur);
+      indexRotation++;
     }
 
+    TriCases(pieceEnCours.cases, 0, pieceEnCours.nbCases - 1);
 
-    while(1)
+    // ==== [Placement de la pièce dans la zone C pour l'affichage] ===
+    for(i = 0; i < pieceEnCours.nbCases; ++i)
+      DessineDiamant(pieceEnCours.cases[i].ligne + 3, pieceEnCours.cases[i].colonne + 14, pieceEnCours.couleur);
+
+
+    SecondRunning = 1;
+
+    while(SecondRunning)
     {
-    
+      estSimilairePiece = true;
+
+      // --- Attend que l'utilisateur insère des diamants égale au nombre de case de la pièce dans la zone C 
+      pthread_mutex_lock(&mutexCasesInserees);
       while(nbCasesInserees < pieceEnCours.nbCases)
+        pthread_cond_wait(&condCasesInserees, &mutexCasesInserees); // Attendre d'être wake up par le le threadEvent
+      pthread_mutex_unlock(&mutexCasesInserees);
+
+      // === [Déterminer le LMin et le CMin de la pièce inséré par l'utilisateur] ===
+      LMin = casesInserees[0].ligne;
+      CMin = casesInserees[0].colonne;
+
+      for(i = 1; i < nbCasesInserees; ++i)
       {
+        if(LMin > casesInserees[i].ligne)
+          LMin = casesInserees[i].ligne;
 
-        pthread_cond_wait(&condCasesInserees, &mutexCaseInserees); // Attendre d'être wake-up par le thread event
+        if(CMin > casesInserees[i].colonne)
+          CMin = casesInserees[i].colonne;
+      }
 
-        for(index = 0; index < nbCasesInserees; index++)
+      pthread_mutex_lock(&mutexCasesInserees);
+
+      // --- Suppression de LMin et CMin pour chaque ligne et colonne
+      for(i = 0; i < nbCasesInserees; i++)
+      {
+        casesInserees[i].ligne -= LMin;
+        casesInserees[i].colonne -= CMin;
+      }
+
+      // --- Tri des différentes cases de la pièce insérée par l'utilisateur
+      TriCases(casesInserees, 0, nbCasesInserees - 1);
+
+      pthread_mutex_unlock(&mutexCasesInserees);
+
+      // --- Vérification de la pièce insérée par l'utilisateur par rapport à la pièce en cours dans la zone C
+      for(i = 0; i < nbCasesInserees; ++i)
+      {
+        if( (casesInserees[i].ligne != pieceEnCours.cases[i].ligne) || (casesInserees[i].colonne != pieceEnCours.cases[i].colonne) )
+          estSimilairePiece = false;
+      }
+
+
+      if(estSimilairePiece)
+      {
+        // --- Dessine la pièce insérée par l'utilisateur en brique bleu --- //
+        for(i = 0; i < nbCasesInserees; i++)
         {
-          if(LMin > casesInserees[index].ligne)   { LMin = casesInserees[index].ligne;   } 
-          if(CMin > casesInserees[index].colonne) { CMin = casesInserees[index].colonne; }
-
+          tab[casesInserees[i].ligne + LMin][casesInserees[i].colonne + CMin] = BRIQUE;
+          DessineBrique(casesInserees[i].ligne + LMin, casesInserees[i].colonne + CMin, false);
         }
 
-        pthread_mutex_lock(&mutexCaseInserees); // Locking
-        for(index = 0; index < nbCasesInserees; ++index)
+        // --- Suppression de la pièce dans la zone C --- //
+        for(i = 0; i < pieceEnCours.nbCases; i++)
         {
-          casesInserees[index].ligne   = casesInserees[index].ligne - LMin;
-          casesInserees[index].colonne = casesInserees[index].colonne - CMin;
+          EffaceCarre(pieceEnCours.cases[i].ligne + 3, pieceEnCours.cases[i].colonne + 14);
         }
-      
-        TriCases(casesInserees, 0, nbCasesInserees);
-        pthread_mutex_unlock(&mutexCaseInserees); 
-      } 
+
+
+        pthread_mutex_lock(&mutexScore);
+        score += pieceEnCours.nbCases;
+        MAJScore = true;
+        pthread_mutex_unlock(&mutexScore);
+
+        pthread_cond_signal(&condScore); // Réveiller le threadScore
+
+        SecondRunning = 0;
+
+      } else { // Dans le cas ou la pièce insérée n'est pas la même que la pièce en cours !
+
+        // Dessine la pièce insérée par l'utilisateur en brique bleu 
+        for(i = 0; i < nbCasesInserees; i++)
+        {
+          tab[casesInserees[i].ligne + LMin][casesInserees[i].colonne + CMin] = VIDE;
+          EffaceCarre(casesInserees[i].ligne + LMin, casesInserees[i].colonne + CMin);
+        }
+
+        // Suppression des lignes et colonnes dans les casesInserees
+        for(i = 0; i < nbCasesInserees; i++)
+        {
+          casesInserees[i].ligne = VIDE;
+          casesInserees[i].colonne = VIDE;
+        }
+
+      } // FIN ELSE
+
+      pthread_mutex_lock(&mutexCasesInserees);
+      nbCasesInserees = 0;
+      pthread_mutex_unlock(&mutexCasesInserees);
+
     }
   }
+  pthread_exit(NULL);
 }
+
 
 /**
  * @brief Fonction permettant d'envoyer un signal suivant le click de la souris.
@@ -359,55 +470,67 @@ void* threadEvent(void)
   sigfillset(&mask); // On masque l'ensemble des signaux
   sigprocmask(SIG_SETMASK, &mask, NULL);
   
-  unsigned short running = 1;
+  unsigned short MainRunning = 1;
+  unsigned short i = 0;
   EVENT_GRILLE_SDL event;
   
   
-  while(running)
+  while(MainRunning)
   {
     event = ReadEvent();
     
     if(event.type == CLIC_GAUCHE)
     {
-      if(tab[event.ligne][event.colonne] == VIDE && nbCasesInserees < NB_CASES)
+      if((tab[event.ligne][event.colonne] == VIDE))
       {
-      
-        DessineDiamant(event.ligne, event.colonne, ROUGE); // Ajout sur le plateau
-        tab[event.ligne][event.colonne] = DIAMANT; // Ajout dans la matrice
-        
-        pthread_mutex_lock(&mutexCaseInserees); // Locking
-        casesInserees[nbCasesInserees].ligne = event.ligne;
-        casesInserees[nbCasesInserees].ligne = event.colonne;
-        
-        
-        
-        nbCasesInserees = nbCasesInserees + 1;
-        pthread_mutex_unlock(&mutexCaseInserees);
- 
-        
-        pthread_cond_signal(&condCasesInserees); // Reveiller ThreadPiece
-      }
+        // --- Remplissage de l'affichage + dans la matrice des cases insérées --- //
+        DessineDiamant(event.ligne,event.colonne,ROUGE);
+        tab[event.ligne][event.colonne] = DIAMANT;
 
+        // --- Protege les variables gobales concernant les cases insérées --- //
+        pthread_mutex_lock(&mutexCasesInserees);
+
+        casesInserees[nbCasesInserees].ligne = event.ligne;
+        casesInserees[nbCasesInserees].colonne = event.colonne;
+
+        nbCasesInserees++;
+
+        pthread_mutex_unlock(&mutexCasesInserees);
+
+        // --- Reveille le threadPiece --- //
+        pthread_cond_signal(&condCasesInserees);
+      }
     }
 
     if(event.type == CLIC_DROIT) // Supprimer DIAMANT
     {
-      if(tab[event.ligne][event.colonne])
+
+      // --- Suppression de l'affichage + dans la matrice des cases insérées --- //
+      for(i = 0; i < nbCasesInserees; i++)
       {
-        
-        tab[event.ligne][event.colonne] = VIDE;
-        EffaceCarre(event.ligne, event.colonne);
-        
-        pthread_mutex_lock(&mutexCaseInserees);
-        nbCasesInserees = nbCasesInserees - 1;
-        pthread_mutex_unlock(&mutexCaseInserees);
+        EffaceCarre(casesInserees[i].ligne, casesInserees[i].colonne);
+        tab[casesInserees[i].ligne][casesInserees[i].colonne] = VIDE;
       }
+      
+      // --- Protege les variables gobales concernant les cases insérées --- //
+      pthread_mutex_lock(&mutexCasesInserees);
+
+      // --- Suppression des lignes et colonnes dans les casesInserees --- //
+      for(i = 0; i < nbCasesInserees; i++)
+      {
+        casesInserees[i].ligne = VIDE;
+        casesInserees[i].colonne = VIDE;
+      }
+  
+      nbCasesInserees = 0;
+      
+      pthread_mutex_unlock(&mutexCasesInserees);
     }
     
     if(event.type == CROIX) {
-      fprintf(stderr, "\x1b[32mVous venez de quitter le jeu !\x1b[0m");
+      fprintf(stderr, "\x1b[32mVous venez de quitter le jeu !\n\x1b[0m");
       kill(getpid(), SIGKILL);
-      running = 0;
+      MainRunning = 0;
     }
     
   }
@@ -425,16 +548,18 @@ void* threadScore(void)
   
   while(1)
   {
-    while(MAJScore == false)
-    {
-      DessineChiffre(1, 14, score);
-      DessineChiffre(1, 15, score);
-      DessineChiffre(1, 16, score);
-      DessineChiffre(1, 17, score);
-
-      pthread_cond_wait(&condScore, &mutexScore);
-    }
     pthread_mutex_lock(&mutexScore); // Locking
+
+    while(!MAJScore)
+    {
+      pthread_cond_wait(&condScore, &mutexScore); // Attendre d'être wake up par le threadPiece
+    }
+
+    DessineChiffre(1, 14, score / 1000);
+    DessineChiffre(1, 15, (score % 1000) / 100);
+    DessineChiffre(1, 16, (score % 100) / 10);
+    DessineChiffre(1, 17, score % 10);
+   
     MAJScore = false;
     pthread_mutex_unlock(&mutexScore);
   }
@@ -531,10 +656,50 @@ void HandlerSIGALARM(int unused)
 
 
 
-
-void HandlerClickGauche(int unused)
+void *threadCases(CASE *pCase)
 {
- 
+
+  pthread_setspecific(cle, (CASE *)pCase);
+
+
+  struct sigaction sigact;  
+  sigact.sa_handler = HandlerSIGUSR1;
+  sigact.sa_flags = 0;
+  
+  
+  sigfillset(&sigact.sa_mask); // On masque l'ensemble des signaux
+  sigdelset(&sigact.sa_mask, SIGUSR1); // On retire du masquage le signal SIGUSR1
+  
+  sigprocmask(SIG_SETMASK, &sigact.sa_mask, NULL);
+  if ((sigaction(SIGALRM, &sigact, 0))==-1)
+    handle_error( "Erreur : sigaction" );
+
+  while(1) {
+    pause();
+  } 
+
+  pthread_exit(NULL);
+}
+
+
+
+void HandlerSIGUSR1(int unused)
+{
+  // Déclaration des variables de travail
+  bool estRempli = true;
+  unsigned int index = 0;
+  CASE *pCase;
+
+  pCase = (CASE *) pthread_getspecific(cle);
+
+
+  for(index = 0; estRempli && index < 9; ++index)
+  {
+    if(tab[pCase->ligne][index] == VIDE)
+      estRempli = false;
+  }
+
+
 }
 
 void HandlerClickDroit(int unused)
